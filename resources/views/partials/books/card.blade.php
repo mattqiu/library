f5991858b997d12db83ffafe8adc26b5d9c4e1c8
曹毅 <shixi_caoyi@staff.sina.com.cn>
2016-03-22 18:41:48 +0800

    add card grid view

---
+++ resources/views/partials/books/card.blade.php
@@ -0,0 +1,7 @@
+<div class="indexBookItem col-md-2 col-sm-3 col-xs-4">
+<div class="my_book_cover">
+      <a href="/book/105"><img src="/uploads/cover/26663519.jpg" class="cover_img"/></a>
+</div>
+<a href="/book/105"><span class="bookTitle">MacTalk 跨越边界</span></a>
+<p>池建强</p>
+</div>
