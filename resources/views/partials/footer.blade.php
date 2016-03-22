d1fdf343be1d4386a680ecad4ecd67fb0472aaf2
曹毅 <shixi_caoyi@staff.sina.com.cn>
2016-03-22 11:44:11 +0800

    add layout and some partials view, and logo img

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,12 @@
+<div id="footer">
+ <div class="container">
+  <div class="text-muted row credit">
+   <div class="col-md-7 links">
+
+   </div>
+   <div class="col-md-5 about">
+    <a href="{{ url('about') }}">关于</a>
+   </div>
+  </div>
+ </div>
+</div>
