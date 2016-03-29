1627358c71992702276b950717740e5f6d2a52f0
曹毅 <shixi_caoyi@staff.sina.com.cn>
2016-03-29 17:21:52 +0800

    touch

--- resources/views/page/user/public.blade.php
+++ resources/views/page/user/public.blade.php
@@ -2,7 +2,7 @@
 
 @section('title',$pageTitle)
 
-@section('contend')
+@section('content')
     <div class="container">
         <div class="col-md-12 col-sm-12 col-xs-12">
             <div class="row">
